f85cbcb093d4c05a2aebeed433373e38c5bb8733
maliang <maliang@lianjia.com>
2018-01-19 11:02:18 +0800

    Initial commit

---
+++ houses/houses/spiders/goufang.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+###引入的是items.py里面构造的类
+# from houses.items import HousesItem
+
+
